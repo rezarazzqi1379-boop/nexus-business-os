6a0c3700b51c90b2a754cb40fb6d03ad4ba5c5bb
rezarazzqi1379-boop <rezarazzqi1379@gmail.com>
2026-08-19 17:45:58 +0330

    refactor(eval): expose canonical promotion layer

--- src/nexus_evals/__init__.py
+++ src/nexus_evals/__init__.py
@@ -7,6 +7,15 @@ from .harness import (
     evaluate_case,
     evaluate_suite,
 )
+from .promotion import (
+    CaseOutcomeMetrics,
+    PromotionDecision,
+    PromotionPolicy,
+    PromotionRun,
+    PromotionSummary,
+    promotion_decision,
+    summarize_promotion_run,
+)
 
 __all__ = [
     "Assertion",
@@ -16,4 +25,11 @@ __all__ = [
     "EvaluationSuiteResult",
     "evaluate_case",
     "evaluate_suite",
+    "CaseOutcomeMetrics",
+    "PromotionDecision",
+    "PromotionPolicy",
+    "PromotionRun",
+    "PromotionSummary",
+    "promotion_decision",
+    "summarize_promotion_run",
 ]

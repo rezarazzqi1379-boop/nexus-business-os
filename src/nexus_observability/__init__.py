0db18ebf4b7f3007784b6e1c3ad742ae6da01437
rezarazzqi1379-boop <rezarazzqi1379@gmail.com>
2026-08-19 18:07:20 +0330

    integration: export observability envelope

---
+++ src/nexus_observability/__init__.py
@@ -0,0 +1,17 @@
+from .events import (
+    EventPrivacy,
+    EventResult,
+    EventType,
+    TraceEvent,
+    TraceValidationResult,
+    validate_trace,
+)
+
+__all__ = [
+    "EventPrivacy",
+    "EventResult",
+    "EventType",
+    "TraceEvent",
+    "TraceValidationResult",
+    "validate_trace",
+]

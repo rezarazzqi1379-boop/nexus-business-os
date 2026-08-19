99b5a35656238d01061aadbb5d164796dde166a3
rezarazzqi1379-boop <rezarazzqi1379@gmail.com>
2026-08-19 15:56:38 +0330

    eval: initialize deterministic evaluation package

---
+++ src/nexus_evals/__init__.py
@@ -0,0 +1,19 @@
+from .harness import (
+    Assertion,
+    AssertionOperator,
+    EvaluationCase,
+    EvaluationResult,
+    EvaluationSuiteResult,
+    evaluate_case,
+    evaluate_suite,
+)
+
+__all__ = [
+    "Assertion",
+    "AssertionOperator",
+    "EvaluationCase",
+    "EvaluationResult",
+    "EvaluationSuiteResult",
+    "evaluate_case",
+    "evaluate_suite",
+]
